34f873a8a30d614889295061a48b97e6a55baa1d
Sergey Vorobiev <littletoss@yandex.ru>
2024-09-19 00:00:29 +0300

    Создал тестовую страницу, добавил переменные для типографики и настроил для них стили, добавил шрифт

--- memuran/php_interface/constants.php
+++ memuran/php_interface/constants.php
@@ -1,7 +1,12 @@
 <?php
 
-$sMainPath = str_replace("\\","/", dirname(__FILE__));
+//$sMainPath = str_replace("\\","/", dirname(__DIR__));
+$sMainPath = "/memuran/";
+
+// Константы
+const CURRENT_TEMPLATE = "base";
 
 // Абсолютные пути к директориям
 define("PATH_MAIN", $sMainPath);
 define("PATH_TEMPLATES", $sMainPath . "/templates/");
+define("PATH_CURRENT_TEMPLATE", $sMainPath . "/templates/" . CURRENT_TEMPLATE . "/");
--- memuran/templates/base/include/footer.php
+++ memuran/templates/base/include/footer.php
@@ -1,1 +1,9 @@
-<?php
+        <section class="section-footer">
+            <div class="container">
+                <div class="footer">
+
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
