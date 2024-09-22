8915564287bb3304106a104c9194975350f7b15b
Sergey Vorobiev <littletoss@yandex.ru>
2024-09-22 19:33:19 +0300

    Закончил верстку детальной + добавил функцию оборачивания таблиц

--- memuran/templates/base/include/footer.php
+++ memuran/templates/base/include/footer.php
@@ -6,4 +6,6 @@
             </div>
         </section>
     </body>
+    <script src="<?=PATH_MAIN . '/js/jquery-3.7.1.min.js'?>"></script>
+    <script src="<?=PATH_MAIN . '/js/main.js'?>"></script>
 </html>
