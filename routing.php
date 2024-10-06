bf4af7fa98f98cf764df2a90551c6f722fe4afb9
Sergey Vorobiev <littletoss@yandex.ru>
2024-10-06 05:30:58 +0300

    Favicon + лого

--- routing.php
+++ routing.php
@@ -19,10 +19,6 @@ switch($sPathUrl){
         require_once __DIR__ . PATH_CURRENT_TEMPLATE . "calendar.php";
         break;
 
-    case "/admin/":
-        require_once __DIR__ . PATH_MAIN . "admin/index.php";
-        break;
-
     default:
         http_response_code(404);
         require_once __DIR__ . PATH_CURRENT_TEMPLATE . "404.php";
